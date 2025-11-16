a67a7dec97eae040e21e657dc7edc193b6ee6093
NTM-2004 <gomeuacong2907@gmail.com>
2025-11-16 20:31:25 +0700

    fix CustomerStat

--- Restaurant/src/java/model/CustomerStat.java
+++ Restaurant/src/java/model/CustomerStat.java
@@ -4,19 +4,19 @@ import java.util.Date;
 
 public class CustomerStat extends Customer{
     private float revenue;
-    private Date date;
+//    private Date date;
 
     public CustomerStat() {}
 
-    public CustomerStat(float revenue, Date date) {
+    public CustomerStat(float revenue) {
         this.revenue = revenue;
-        this.date = date;
+//        this.date = date;
     }
 
-    public CustomerStat(float revenue, Date date, int id, String name, String phone, String email) {
+    public CustomerStat(float revenue, int id, String name, String phone, String email) {
         super(id, name, phone, email);
         this.revenue = revenue;
-        this.date = date;
+//        this.date = date;
     }
 
     public float getRevenue() {
@@ -27,12 +27,12 @@ public class CustomerStat extends Customer{
         this.revenue = revenue;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
+//    public Date getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(Date date) {
+//        this.date = date;
+//    }
     
 }
